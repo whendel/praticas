e4383cb92bec8ad9bdbd2bc0474862374d469c35
whendel <whendel.s.a.wsa@gmail.com>
2022-12-13 23:52:36 -0300

    first commit

---
+++ facebook.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Facebook</title>
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+	<header>
+			<h1>facebook</h1>
+			<p>O Facebook ajuda você a se conectar e<br> compartilhar com as pessoas que<br> fazem parte da sua vida.</p>
+	</header>
+
+	<div class="container">
+			<form>
+				<div class="input1">
+					<input class="inputtext" type="text" id="email" name="email" placeholder="Email ou telefone">
+				</div>
+				<div class="input2">	
+					<input class="inputpassword" type="password" id="senha" name="senha" placeholder="Senha">
+				</div>
+				<div class="submit1">
+					<button class="botao" name="login" type="submit">Entrar</button>
+				</div>	
+				<div class="link-esqueceu-senha">	
+					<a href="">Esqueceu a senha?</a>
+				</div>
+				
+					<input class="submit2" type="submit" value="Criar nova conta">
+			</form>			
+	</div>
+
+			<div class="paragrafo">
+					<p><a href=""><strong>Criar uma Página</strong></a> para uma celebridade, uma marca ou uma empresa.</p>
+			</div>	
+
+	<div class="rodape"> 
+		<div class="footer1">
+				<p><a href="">Português (Brasil)</a> <a href="">English (US)</a> <a href="">Español</a> <a href="">Français (France)</a> <a href="">Italiano Deutsch</a> <a href="">العربية </a> <a href=""><हिन्दी</a> <a href="">中文(简体)</a> <a href="">日本語</a>		
+				</p>
+		</div>
+		<div class="footer2">
+				<p><a href="">Cadastre-se</a> <a href="">Entrar</a> <a href="">Messenger<a href=""> <a href="">Facebook Lite</a> <a href="">Watch</a> <a href="">Locais</a> <a href="">Jogos</a> <a href="">Marketplace</a>Meta Pay</a> <a href="">Oculus</a> <a href="">Portal</a> <a href="">Instagram</a>B<a href="">oletim</a> <a href="">Campanhas de arrecadação de fundos</a> <a href="">Serviços</a> <a href="">Central de Informações de Votação</a> <a href="">Grupos</a> <a href="">Sobre</a> <a href="">Criar anúncio</a> <a href="">Criar Página</a> <a href="">Desenvolvedores</a> <a href="">Carreiras</a> <a href="">Política de Privacidade</a> <a href="">Central de Privacidade</a> <a href="">Cookies</a> <a href="">Escolhas para anúncios</a> <a href="">Termos</a> <a href="">Ajuda</a> <a href="">Carregamento de contatos e não usuários</a></p>
+				<p>Meta © 2022</p>
+		</div>			
+    </div>
+</body>
+</html>
